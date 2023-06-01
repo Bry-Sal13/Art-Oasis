073b7e855ff7da2f1ae508dc6e4ba0cf20a2ba99
Justin <brayshawjustin@gmail.com>
2023-06-01 15:20:21 -0700

    pushed for others to pull

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -40,69 +40,117 @@ function Nav({users, getUserData}) {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-pastel-blue">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <a className="navbar-brand ps-4" href="index.html">Art Oasis</a>
+      <NavLink className="navbar-brand ps-4" to="/">
+        Art Oasis
+      </NavLink>
 
       <div className="search">
-        <form  onSubmit={handleSearch} className="d-flex" role="search">
-          <input onChange={handleSearchChange} className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-light" type="submit">Search</button>
+        <form onSubmit={handleSearch} className="d-flex" role="search">
+          <input
+            onChange={handleSearchChange}
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Search
+          </button>
         </form>
-        {filteredUsers.length !== 0 &&(
-        <div className="dataResult">
-          {filteredUsers.slice(0, 15).map((user) => {
-            return (
-              <div className='search-result-margins' key={user.user_id}>
-                <NavLink to={`/api/users/${user.username}`} className="dataItem" target="_blank" rel='noreferrer'>
-                  <img className="left-align" src={user.profile_picture} alt="Profile" />
-                  <p className='text-align-right'>{user.display_name}</p>
-                </NavLink>
-              </div>
-            );
+        {filteredUsers.length !== 0 && (
+          <div className="dataResult">
+            {filteredUsers.slice(0, 15).map((user) => {
+              return (
+                <div className="search-result-margins" key={user.user_id}>
+                  <NavLink
+                    to={`/api/users/${user.username}`}
+                    className="dataItem"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="left-align"
+                      src={user.profile_picture}
+                      alt="Profile"
+                    />
+                    <p className="text-align-right">{user.display_name}</p>
+                  </NavLink>
+                </div>
+              );
             })}
-        </div>
+          </div>
         )}
       </div>
 
-      <div className="collapse navbar-collapse right-align" id="navbarTogglerDemo03">
+      <div
+        className="collapse navbar-collapse right-align"
+        id="navbarTogglerDemo03"
+      >
         <ul className="navbar-nav ms-auto mt-3 mt-lg-0 pe-4">
           <li className="nav-item px-2">
-            <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+            <NavLink className="nav-link" aria-current="page" to="/">
+              Home
+            </NavLink>
           </li>
 
           <li className="nav-item px-2">
-            <NavLink className="nav-link" aria-current="page" to="/connections">Connections</NavLink>
+            <NavLink className="nav-link" aria-current="page" to="/connections">
+              Connections
+            </NavLink>
           </li>
 
           <li className="nav-item px-2">
-            <NavLink className="nav-link" aria-current="page" to="/profile">Profile</NavLink>
+            <NavLink className="nav-link" aria-current="page" to="/profile">
+              Profile
+            </NavLink>
           </li>
 
           <div className="d-flex justify-content-between">
-
             {/* if token is not valid show */}
-            { loggedIn === false && (
+            {loggedIn === false && (
               <>
                 <li className="nav-item px-2">
-                    <NavLink className="nav-link" aria-current="page" to="/signup">Signup</NavLink>
+                  <NavLink
+                    className="nav-link"
+                    aria-current="page"
+                    to="/signup"
+                  >
+                    Signup
+                  </NavLink>
                 </li>
 
                 <li className="nav-item px-2">
-                  <NavLink className="nav-link" aria-current="page" to="/login">Login</NavLink>
+                  <NavLink className="nav-link" aria-current="page" to="/login">
+                    Login
+                  </NavLink>
                 </li>
               </>
             )}
 
             {/* if token is valid show */}
-            { loggedIn !== false && (
-            <li className="nav-item px-2">
-              <button onClick={handleLogout} className="btn btn-outline-light">Logout</button>
-            </li>
+            {loggedIn !== false && (
+              <li className="nav-item px-2">
+                <button
+                  onClick={handleLogout}
+                  className="btn btn-outline-light"
+                >
+                  Logout
+                </button>
+              </li>
             )}
           </div>
-          </ul>
+        </ul>
       </div>
     </nav>
   );
