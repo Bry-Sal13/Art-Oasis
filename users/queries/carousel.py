62aeb9434fd6c979c90e84c295a4c6ab6f5d3898
Bryant Salazar <bryantsalazar2011@gmail.com>
2023-06-06 20:50:49 +0000

    Resolve "Deployment"

--- users/queries/carousel.py
+++ users/queries/carousel.py
@@ -1,10 +1,11 @@
+from typing import List, Union
 from pydantic import BaseModel
-from typing import List,  Union, Optional
 from psycopg_pool import ConnectionPool
 from fastapi import HTTPException, status
 import os
 
 pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
+
 
 class Error(BaseModel):
     message: str
@@ -35,21 +36,21 @@ class CarouselRepository:
                         [carousel_id],
                     )
                     if cur.rowcount <= 0:
-                        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Carousel not found")
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Carousel not found",
+                        )
                     record = cur.fetchone()
                     if record is None:
                         return None
                     return CarouselOut(
-                        id=record[0],
-                        user_id=record[1],
-                        link=record[2]
+                        id=record[0], user_id=record[1], link=record[2]
                     )
         except HTTPException:
             raise
         except Exception as e:
             print(e)
             return {"Message": "Something went wrong"}
-
 
     def get_carousels(self) -> Union[List[CarouselOut], Error]:
         try:
@@ -64,9 +65,7 @@ class CarouselRepository:
                     results = []
                     for record in cur:
                         carousel = CarouselOut(
-                            id=record[0],
-                            user_id=record[1],
-                            link=record[2]
+                            id=record[0], user_id=record[1], link=record[2]
                         )
                         results.append(carousel)
                     return results
@@ -74,8 +73,7 @@ class CarouselRepository:
             print(e)
             return {"Message": "Something went wrong"}
 
-
-    def create_carousel(self, data: CarouselIn) -> Union[CarouselOut,  Error]:
+    def create_carousel(self, data: CarouselIn) -> Union[CarouselOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as cur:
@@ -99,16 +97,13 @@ class CarouselRepository:
             print(e)
             return {"message": "Could not create a carousel with that data"}
 
-
-    def update_carousel(self, carousel_id: int, data: CarouselIn) -> Union[CarouselOut, Error]:
+    def update_carousel(
+        self, carousel_id: int, data: CarouselIn
+    ) -> Union[CarouselOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as cur:
-                    params = [
-                        data.user_id,
-                        data.link,
-                        carousel_id
-                    ]
+                    params = [data.user_id, data.link, carousel_id]
                     cur.execute(
                         """
                         UPDATE image_carousel
@@ -119,7 +114,10 @@ class CarouselRepository:
                         params,
                     )
                     if cur.rowcount <= 0:
-                        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Carousel not found")
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Carousel not found",
+                        )
                     data = data.dict()
                     data["id"] = carousel_id
                     return CarouselOut(**data)
@@ -128,7 +126,6 @@ class CarouselRepository:
         except Exception as e:
             print(e)
             return {"message": "Could not update a carousel with that ID"}
-
 
     def delete_carousel(self, carousel_id: int) -> Union[bool, Error]:
         try:
@@ -139,10 +136,13 @@ class CarouselRepository:
                         DELETE FROM image_carousel
                         WHERE id = %s
                         """,
-                        [carousel_id]
+                        [carousel_id],
                     )
                     if cur.rowcount <= 0:
-                        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Carousel not found")
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Carousel not found",
+                        )
                     return True
         except HTTPException:
             raise
--- users/routers/users.py
+++ users/routers/users.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 from fastapi import (
     Depends,
     APIRouter,
@@ -6,14 +7,12 @@ from fastapi import (
     Response,
     Request,
 )
-from typing import List, Union
 from pydantic import BaseModel
 from jwtdown_fastapi.authentication import Token
 from authenticator import authenticator
 from queries.users import (
     UserIn,
     UserOut,
-    UserOutWithPassword,
     UserRepository,
     Error,
 )
@@ -34,13 +33,13 @@ class HttpError(BaseModel):
 
 router = APIRouter()
 
-    # user_data: dict = Depends(authenticator.get_current_account_data),
+# user_data: dict = Depends(authenticator.get_current_account_data),
 
 
 @router.get("/token", response_model=UserToken | None)
 async def get_token(
     request: Request,
-    user: UserOut = Depends(authenticator.try_get_current_account_data)
+    user: UserOut = Depends(authenticator.try_get_current_account_data),
 ) -> UserToken | None:
     if authenticator.cookie_name in request.cookies:
         return {
@@ -74,20 +73,8 @@ async def create_user(
 async def get_users(
     repo: UserRepository = Depends(),
     user_data: dict = Depends(authenticator.get_current_account_data),
-    
 ):
     return repo.get_users()
-
-
-# @router.get("/api/users/{username}/d", response_model=UserOutWithPassword | HttpError)
-# async def get_one_user(
-#     username: str,
-#     response: Response,
-#     repo: UserRepository = Depends()
-# ) -> UserOutWithPassword:
-#     if not repo.get_user(username):
-#         response.status_code = 404
-#     return repo.get_user(username)
 
 
 @router.get("/api/users/{username}", response_model=UserOut | HttpError)
