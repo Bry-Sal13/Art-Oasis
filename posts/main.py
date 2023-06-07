ed3e986f1f3ab38ef6831184036e2129a282407f
Bryant Salazar <bryantsalazar2011@gmail.com>
2023-06-06 21:00:04 -0600

    added to main.py something i forgot

--- posts/main.py
+++ posts/main.py
@@ -22,16 +22,8 @@ app.add_middleware(
 
 
 @app.get("/")
-def launch_details():
-    return {
-        "launch_details2": {
-            "module": 3,
-            "week": 17,
-            "day": 5,
-            "hour": 19,
-            "min": "00",
-        }
-    }
+def root():
+    return {"message": "Hello World"}
 
 
 @app.get("/api/launch-details")
