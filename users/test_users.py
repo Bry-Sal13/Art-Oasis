a404f9bb81139214ba0615a27247f5bf72f8e296
Justin <brayshawjustin@gmail.com>
2023-06-07 17:52:08 -0700

    formatting

--- users/test_users.py
+++ users/test_users.py
@@ -1,7 +1,7 @@
 from main import app
 from fastapi.testclient import TestClient
 from authenticator import authenticator
-from queries.users import UserOut, UserIn, UserOutWithPassword, UserRepository
+from queries.users import UserOut, UserRepository
 
 
 client = TestClient(app)
@@ -11,60 +11,63 @@ def test_init():
     assert 1 == 1
 
 
-## TEST CREATE USER ##
+# TEST CREATE USER
 def fake_get_current_account_data():
     return UserOut(
-        user_id= 1,
+        user_id=1,
         email="krze@gmail.com",
-        profile_picture= "test pfp",
-        display_name = "Eim_Krze",
+        profile_picture="test pfp",
+        display_name="Eim_Krze",
         header_image="test header image",
         first_name="Eim",
         last_name="Krze",
         username="Krze",
         category="krze art",
-        about="krze art dude"
+        about="krze art dude",
     )
 
+
 class fakeUserRepo:
-    def fe_get_user(self, username: str):
+    def fe_get_user(self):
         return UserOut(
-            user_id= 1,
+            user_id=1,
             email="krze@gmail.com",
-            profile_picture= "test pfp",
-            display_name = "Eim_Krze",
+            profile_picture="test pfp",
+            display_name="Eim_Krze",
             header_image="test header image",
             first_name="Eim",
             last_name="Krze",
             username="Krze",
             category="krze art",
-            about="krze art dude"
+            about="krze art dude",
         )
 
+
 def test_get_user():
-    # Arrange 
+    # Arrange
     app.dependency_overrides[UserRepository] = fakeUserRepo
-    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
-    
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_get_current_account_data
+
     # Act
     response = client.get("/api/users/Krze")
-    
+
     # Clean up
     app.dependency_overrides = {}
-    
+
     # Assert
     assert response.status_code == 200
     assert response.json() == UserOut(
-        user_id= 1,
+        user_id=1,
         email="krze@gmail.com",
-        profile_picture= "test pfp",
-        display_name = "Eim_Krze",
+        profile_picture="test pfp",
+        display_name="Eim_Krze",
         header_image="test header image",
         first_name="Eim",
         last_name="Krze",
         username="Krze",
         category="krze art",
-        about="krze art dude"
+        about="krze art dude",
     )
-
 
