37e46753d5ae1cec716cc17f51d0206073fbeb8c
Justin <brayshawjustin@gmail.com>
2023-06-08 09:56:23 -0700

    created update_user unit test

--- users/test_users.py
+++ users/test_users.py
@@ -1,7 +1,7 @@
 from main import app
 from fastapi.testclient import TestClient
 from authenticator import authenticator
-from queries.users import UserOut, UserRepository
+from queries.users import UserOut, UserIn, UserRepository
 
 
 client = TestClient(app)
@@ -27,7 +27,7 @@ def fake_get_current_account_data():
     )
 
 
-class fakeUserRepo:
+class FakeUserRepo:
     def fe_get_user(self, username: str):
         return UserOut(
             user_id=1,
@@ -42,10 +42,74 @@ class fakeUserRepo:
             about="krze art dude",
         )
 
+    def update_user(self, username: str, data: UserIn):
+        return UserOut(
+            user_id=1,
+            email="krze@gmail.com",
+            profile_picture="Crze pfp",
+            display_name="Eim_Crze",
+            header_image="test header image",
+            first_name="Eim",
+            last_name="Crze",
+            username="Krze",
+            category="Crze art",
+            about="Crze art dude",
+        )
+
+
+def test_update_user():
+    # Arrange
+    app.dependency_overrides[UserRepository] = FakeUserRepo
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_get_current_account_data
+
+    # variable for the url
+    username = "Krze"
+
+    # EXPECTED RESULT
+    updated_fake_user = UserOut(
+        user_id=1,
+        email="krze@gmail.com",
+        profile_picture="Crze pfp",
+        display_name="Eim_Crze",
+        header_image="test header image",
+        first_name="Eim",
+        last_name="Crze",
+        username="Krze",
+        category="Crze art",
+        about="Crze art dude",
+    )
+
+    # Act
+    response = client.put(
+        f"/api/users/{username}",
+        headers={"Content-Type": "application/json"},
+        json={
+            "user_id": 1,
+            "email": "krze@gmail.com",
+            "profile_picture": "Crze pfp",
+            "display_name": "Eim_Crze",
+            "header_image": "test header image",
+            "first_name": "Eim",
+            "last_name": "Crze",
+            "username": "Krze",
+            "category": "Crze art",
+            "about": "Crze art dude",
+        },
+    )
+
+    # Clean up
+    app.dependecy_overrides = {}
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == updated_fake_user
+
 
 def test_get_user():
     # Arrange
-    app.dependency_overrides[UserRepository] = fakeUserRepo
+    app.dependency_overrides[UserRepository] = FakeUserRepo
     app.dependency_overrides[
         authenticator.get_current_account_data
     ] = fake_get_current_account_data
