fff0db6652297e30e84da6f8a13eb4001daa70a5
Justin <brayshawjustin@gmail.com>
2023-06-08 12:23:07 -0700

    linting

--- users/queries/users.py
+++ users/queries/users.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
 from typing import List, Optional, Union
+from pydantic import BaseModel
 from psycopg_pool import ConnectionPool
 from fastapi import HTTPException, status
-
 
 pool = ConnectionPool("postgresql://users:password@postgres:5432/users")
 
