a837653cb8ceab60efa00df15fc67642b9dc6c3b
Justin <brayshawjustin@gmail.com>
2023-06-08 21:34:16 -0700

    consolidating unit tests

--- users/test_users.py
+++ users/test_users.py
@@ -1,6 +1,8 @@
 from main import app
 from fastapi.testclient import TestClient
 from authenticator import authenticator
+from queries.posts import PostRepository, PostOut
+from datetime import datetime
 from queries.users import UserOut, UserIn, UserRepository
 
 
@@ -191,5 +193,61 @@ def test_get_users():
     response = client.get("/api/users")
     app.dependency_overrides = {}
     # Assert
+    assert response.status_code == 200
+    assert response.json() == desired_result
+
+
+## BRYANT'S UNIT TESTS
+
+current_time = datetime.now()
+
+
+# TEST CREATE POST
+def fake_user():
+    return UserOut(
+        user_id=1,
+        email="brysal@gmail.com",
+        profile_picture="test pfp",
+        display_name="Bry_Sal",
+        header_image="test header image",
+        first_name="Bry",
+        last_name="Sal",
+        username="Brysal",
+        category="Bry art",
+        about="Bry making art",
+    )
+
+
+class EmptyPostRepository:
+    def get_one(self, post_id: int):
+        return PostOut(
+            id=1,
+            username="Brysal",
+            text="hello",
+            image="default image",
+            created=current_time,
+        )
+
+
+def test_get_post():
+    # ARRANGE
+    app.dependency_overrides[PostRepository] = EmptyPostRepository
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_user
+    id = 1
+    desired_result = {
+        "id": 1,
+        "username": "Brysal",
+        "text": "hello",
+        "image": "default image",
+        "created": current_time.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+    }
+
+    response = client.get(f"/api/posts/{id}")
+    app.dependency_overrides = {}
+    print(response.json())
+    print(desired_result)
+    # ASSERT
     assert response.status_code == 200
     assert response.json() == desired_result
