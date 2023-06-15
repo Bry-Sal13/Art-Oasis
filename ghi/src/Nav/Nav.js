2e14d99ff90116b9c425907fb1f5e2ae88df855c
Zach Winter <z.winter2121@gmail.com>
2023-06-14 17:40:24 -0700

    added new icon to tab, a bunch of rando css, fixed search bar not dissapearing after losing focus, added button to toggle comment section

--- ghi/src/Nav/Nav.js
+++ ghi/src/Nav/Nav.js
@@ -86,19 +86,28 @@ function Nav({ users, token, setUserInfo, getUser, setUser }) {
 
             <div className="search">
                 <form onSubmit={handleSearch} role="search">
-                    <input
-                        onChange={handleSearchChange}
-                        className="form-control me-2 d-inline"
-                        type="search"
-                        id="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                    />
-                    <button
-                        className="btn btn-outline-light position-absolute"
-                        type="submit">
-                        Search
-                    </button>
+                    <div className="input-container">
+                        <input
+                            onChange={handleSearchChange}
+                            className="form-control me-2 d-inline"
+                            type="search"
+                            id="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button className="search-btn" type="submit">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="white"
+                                className="bi bi-search sign"
+                                viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            </svg>
+                            <div className="search-text">Search</div>
+                        </button>
+                    </div>
                 </form>
                 {filteredUsers.length !== 0 && (
                     <div className="dataResult">
@@ -119,7 +128,6 @@ function Nav({ users, token, setUserInfo, getUser, setUser }) {
                                                     height: "48px",
                                                 }}
                                             />
-
                                             <p className="text-align-right mb-0">
                                                 {user.display_name}
                                             </p>
